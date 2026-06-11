9067933d04c031021afcbfed5c1d8e8ee8a0681a
kuruae <kuruu@protonmail.com>
2026-06-11 21:47:52 +0200

    added cache line padding and a main test

--- includes/SPSC.hpp
+++ includes/SPSC.hpp
@@ -1,17 +1,22 @@
 #include <array>
 #include <atomic>
 #include <cstddef>
+#include <new>
 
-template <typename T, size_t Capacity> class SPCS {
+template <typename T, size_t Capacity> class SPSC {
 private:
-  std::array<T, Capacity> buffer_;
+  alignas(std::hardware_destructive_interference_size)
+      std::array<T, Capacity> buffer_;
 
-  alignas(64) std::atomic_size_t head_{0};
-  alignas(64) std::atomic_size_t tail_{0};
+  alignas(std::hardware_destructive_interference_size) std::atomic_size_t head_{
+      0};
+
+  alignas(std::hardware_destructive_interference_size) std::atomic_size_t tail_{
+      0};
 
 public:
   bool push(T &&item) {
-    const size_t t = tail_.load(std::memory_order_acquire);
+    const size_t t = tail_.load(std::memory_order_relaxed);
     const size_t h = head_.load(std::memory_order_acquire);
 
     if ((t - h) == Capacity)
@@ -24,7 +29,7 @@ public:
   }
 
   bool pop(T &item) {
-    const size_t h = head_.load(std::memory_order_acquire);
+    const size_t h = head_.load(std::memory_order_relaxed);
     const size_t t = tail_.load(std::memory_order_acquire);
 
     if (t == h)
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,39 @@
-#include "../includes/spsc.hpp"
+#include "../includes/SPSC.hpp"
+#include <cassert>
+#include <cstddef>
 #include <print>
+#include <thread>
 
 int main() {
-  std::println("Hello World!");
+  constexpr size_t n{1000000};
+  SPSC<size_t, 1024> queue;
+
+  size_t produced_sum{};
+  size_t consumed_sum{};
+
+  std::thread producer([&] {
+    for (size_t i{}; i < n; i++) {
+      produced_sum += i;
+      while (!queue.push(static_cast<size_t>(i)))
+        ;
+    }
+  });
+
+  std::thread consumer([&] {
+    size_t received{};
+    while (received < n) {
+      size_t val{};
+      if (queue.pop(val)) {
+        consumed_sum += val;
+        received++;
+      }
+    }
+  });
+
+  producer.join();
+  consumer.join();
+
+  assert(produced_sum == consumed_sum);
+  std::println("Correctness OK: {} messages", n);
   return 0;
 }
