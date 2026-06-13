8f30b2db970ebe74272e5e3948c1661dc5be8528
kuruae <kuruu@protonmail.com>
2026-06-14 00:11:26 +0200

    benchmarking

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,92 @@
 #include "../includes/FS_SPSC.hpp"
 #include "../includes/MTX_SPSC.hpp"
 #include "../includes/SPSC.hpp"
+#include <algorithm>
+#include <atomic>
 #include <cassert>
-#include <cstddef>
+#include <cstdint>
 #include <print>
 #include <thread>
+#include <vector>
 
-static constexpr size_t n{1000000};
+static constexpr size_t N{10'100'000};
 
-template <typename Qtype> void runTest() {
+inline void pin_thread(int core_id) {
+  cpu_set_t cpuset;
+  CPU_ZERO(&cpuset);
+  CPU_SET(core_id, &cpuset);
+  pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset);
+}
+
+inline void inline_pause() { __builtin_ia32_pause(); }
+
+[[nodiscard]]
+inline uint64_t rdtsc_start() {
+  uint32_t lo, hi;
+  __asm__ volatile("lfence\n\trdtsc" : "=a"(lo), "=d"(hi));
+  return static_cast<uint64_t>(hi) << 32 | lo;
+}
+
+[[nodiscard]]
+inline uint64_t rdtsc_end() {
+  uint32_t lo, hi, aux;
+  __asm__ volatile("rdtscp\n\tlfence" : "=a"(lo), "=d"(hi), "=c"(aux));
+  return static_cast<uint64_t>(hi) << 32 | lo;
+}
+
+template <typename Qtype> void runTest(const std::string &test_name) {
   Qtype queue;
-  size_t produced_sum{};
-  size_t consumed_sum{};
+  std::vector<uint64_t> latencies(N, 0);
+  std::atomic<bool> start_signal{false};
 
-  std::thread producer([&] {
-    for (size_t i{}; i < n; i++) {
-      produced_sum += i;
-      while (!queue.push(static_cast<size_t>(i)))
-        ;
+  std::jthread producer([&] {
+    pin_thread(2);
+
+    while (!start_signal.load(std::memory_order_acquire)) {
+      inline_pause();
+    }
+
+    for (size_t i{}; i < N; i++) {
+      uint64_t timestamp{rdtsc_start()};
+
+      while (!queue.push(std::move(timestamp))) {
+        inline_pause();
+        timestamp = rdtsc_start();
+      }
+
+      uint64_t pace_until = rdtsc_start() + 3000;
+      while (rdtsc_start() < pace_until) {
+        inline_pause();
+      }
     }
   });
 
-  std::thread consumer([&] {
-    size_t received{};
-    while (received < n) {
-      size_t val{};
-      if (queue.pop(val)) {
-        consumed_sum += val;
-        received++;
+  std::jthread consumer([&] {
+    pin_thread(4);
+
+    start_signal.store(true, std::memory_order_release);
+
+    for (size_t i = 0; i < N; i++) {
+      uint64_t sent{};
+      while (!queue.pop(sent)) {
+        inline_pause();
       }
+      uint64_t end_time = rdtsc_end();
+
+      latencies[i] = (end_time > sent) ? (end_time - sent) : 0;
     }
   });
 
   producer.join();
   consumer.join();
 
-  assert(produced_sum == consumed_sum);
+  std::ranges::sort(latencies);
+
+  std::println("[{}]", test_name);
+  std::println("  p50  :   {} cycles", latencies[N * 50 / 100]);
+  std::println("  p99  :   {} cycles", latencies[N * 99 / 100]);
+  std::println("  p999 :   {} cycles", latencies[N * 999 / 1000]);
+  std::println("-----------------");
 }
 
 /**
@@ -50,14 +101,11 @@ template <typename Qtype> void runTest() {
  * Lock-based queue
  */
 int main() {
-  runTest<SPSC<size_t, 1024>>();
-  std::println("[MAIN SPSC]: Correctness OK: {} messages\n", n);
+  pin_thread(0);
 
-  runTest<FS_SPSC<size_t, 1024>>();
-  std::println("[FS SPSC]: Correctness OK: {} messages\n ", n);
-
-  runTest<MTX_SPSC<size_t, 1024>>();
-  std::println("[MUTEX SPSC]: Correctness OK: {} messages\n ", n);
+  runTest<SPSC<size_t, 1024>>("MAIN SPSC");
+  runTest<FS_SPSC<size_t, 1024>>("FS SPSC");
+  runTest<MTX_SPSC<size_t, 1024>>("MUTEX SPSC");
 
   return 0;
 }
